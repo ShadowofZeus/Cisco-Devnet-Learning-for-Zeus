4fdcdc90d7572b733bb1046acf68b75aa6d733ee
ShadowofZeus <simbateabag@gmail.com>
2019-11-14 19:19:08 +0200

    Head First Chapter 4

--- Nester/nester.py
+++ Nester/nester.py
@@ -2,11 +2,12 @@
 This is the print lists function which prints the contents of any list
 """
 
-def print_lists(AnyList,level=0):
+def print_lists(AnyList,level=0,indent=False,fh=sys.stdout):
     for each_item in AnyList:
         if isinstance(each_item,list):
-            print_lists(each_item,level+1)
+            print_lists(each_item,level+1,indent,fh)
         else:
-            for tab_stop in range(level):
-                print("\t",end="")
-                print(each_item)
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end="",file=fh)
+                    print(each_item,file=fh)
