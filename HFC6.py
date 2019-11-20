6eb7a713a148caaf9d858f91a1a4ec2bc1dc772a
ShadowofZeus <simbateabag@gmail.com>
2019-11-20 17:57:35 +0200

    Python in 7

--- HFC6.py
+++ HFC6.py
@@ -7,6 +7,18 @@ class Athlete:
         self.times = a_times
 
 """
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter='-'
+    elif ':' in time_string:
+        splitter=':'
+    else:
+        return(time_string)
+    (mins,secs)=time_string.split(splitter)
+
+    return(mins + '.' + secs)
+
+
 class AthleteList(list):
     def __init__(self, a_name, a_dob=None, a_times=[]):
         list._init_([])
@@ -14,8 +26,9 @@ class AthleteList(list):
         self.dob = a_dob
         self.extend(a_times)
 
-def top3(self):
-    return(sorted(set([sanitize(t) for t in self.times]))[0:3])
+    def top3(self):
+        return(sorted(set([sanitize(t) for t in self]))[0:3])
+
 
 def get_coach_data(filename):
     try:
@@ -28,30 +41,19 @@ def get_coach_data(filename):
         print('File error: ' + str(ioerr))
         return(None)
 
-
-def sanitize(time_string):
-    if '-' in time_string:
-        splitter='-'
-    elif ':' in time_string:
-        splitter=':'
-    else:
-        return(time_string)
-    (mins,secs)=time_string.split(splitter)
-
-    return(mins + '.' + secs)
-
+"""                                 #Not needed anymore as List class has been inherited.
 def add_time(self,time_value):
     self.times.append(time_value)
 
 def add_times(self,list_of_times):
     self.times.extend(list_of_times)
-
+"""
 james=get_coach_data('james2.txt')
-julie=get_coach_data('julie2.txt')
-mikey=get_coach_data('mikey2.txt')
-sarah=get_coach_data('sarah2.txt')
+julie=get_coach_data('julie')
+mikey=get_coach_data('mikey2')
+sarah=get_coach_data('sarah2')
 
-print(james.name + "'s fastest times are: " + str(top3(james)))
-print(julie.name + "'s fastest times are: " + str(top3(julie)))
-print(mikey.name + "'s fastest times are: " + str(top3(mikey)))
-print(sarah.name + "'s fastest times are: " + str(top3(sarah)))
+print(james.name + "'s fastest times are: " + str(james.top3()))
+print(julie.name + "'s fastest times are: " + str(julie.top3()))
+print(mikey.name + "'s fastest times are: " + str(mikey.top3()))
+print(sarah.name + "'s fastest times are: " + str(sarah.top3()))
---
+++ Python-In-7/grades.py
@@ -0,0 +1,9 @@
+num=float(input("Enter your kid's grade: "))
+if num > 4:
+    print("the grade is A")
+elif num > 3:
+    print("the grade is B")
+elif num > 2:
+    print("the grade is C")
+else:
+    print("Your kid doesnt deserve to be at this school")
