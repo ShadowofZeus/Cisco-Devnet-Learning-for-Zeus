f2f872be7b54566ed93f9c736ae3b8233fcf90ab
ShadowofZeus <simbateabag@gmail.com>
2019-11-23 09:50:40 +0200

    Chapter 11 on Object Oriented programming

--- Python-In-7/unpickling.py
+++ Python-In-7/unpickling.py
@@ -1,6 +1,6 @@
 import pickle
 
-pickle_file = open("emp1.dat",'r')
+pickle_file = open("emp1.dat",'rb')
 name_list = pickle.load(pickle_file)
 skill_list =pickle.load(pickle_file)
-print(name_list ,"n", skill_list)
+print(name_list ,"and", skill_list)
