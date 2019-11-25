6bd51491906569da37353b33485dfa46a5e207e0
ShadowofZeus <simbateabag@gmail.com>
2019-11-25 08:41:11 +0200

    Class methods, private variables and operator overloading

---
+++ Python-In-7/override-mthd.py
@@ -0,0 +1,16 @@
+class A():
+    def sum1(self,a,b):
+        print("In class A")
+        c = a+b
+        return c
+class B(A):
+    """
+    def sum1(self,a,b):
+        print ("In class B")
+        c= a*a+b*b
+        return c
+"""   # Uses Class A if this function is unavailable
+
+b_obj = B()
+print (B.__dict__)
+print (b_obj.sum1(4,5))
